41329b4cf4d7b14c23b13a332a3dd2b7f935bddd
lnroma <family_89@mail.ru>
2017-07-29 00:03:22 +0300

    alert no search

--- resources/views/anketa/list.blade.php
+++ resources/views/anketa/list.blade.php
@@ -4,11 +4,11 @@
     {!! Breadcrumbs::render('dating') !!}
     @include('layouts.snipets.error')
     <h2>Знакомства</h2>
-    <span>
         @include('anketa.element.filters')
-    </span>
-    <br/>
-    &nbsp; <br>
+    <br/>&nbsp;<br/>
+    <?php if($ankets->count() == 0): ?>
+    <div class="alert alert-danger">По вашему запросу анкеты не найдены</div>
+    <?php endif; ?>
     @foreach($ankets as $_ankets)
         @include('anketa.element.anketa', array('anketa' => $_ankets))
     @endforeach
