4d1bd695c134b6341720ec3f509671e6cdea6b57
lnroma <family_89@mail.ru>
2017-10-11 21:43:54 +0300

    message angular

--- public/js/controllers/MessageController.js
+++ public/js/controllers/MessageController.js
@@ -8,10 +8,37 @@ myApp.controller('MessageController',  function($scope) {
         html = html.replaceAll(':-)', '<img src="/smiles/smiles/smile.gif"/>');
         html = html.replaceAll('[b]', '<b>');
         html = html.replaceAll('[/b]', '</b>');
-        html = html.replaceAll('[s]', '<s>');
-        html = html.replaceAll('[/s]', '</s>');
+        html = html.replaceAll('[s]', '<strike>');
+        html = html.replaceAll('[/s]', '</strike>');
+        html = html.replaceAll('[i]', '<i>');
+        html = html.replaceAll('[/i]', '</i>');
         content = html;
         return content;
+    };
+    
+    $scope.bold = function () {
+        inputTags('[b]', '[/b]');
+    };
+
+    $scope.italic = function () {
+        inputTags('[i]', '[/i]');
+    };
+
+    $scope.strice = function () {
+        inputTags('[s]', '[/s]')
+    };
+    
+    inputTags = function (tag1, tag2) {
+
+        var allMessage = $('#message').val();
+        var positionStart = $('#message').prop('selectionStart');
+        var positionEnd = $('#message').prop('selectionEnd');
+
+        var startString = allMessage.substring(0, positionStart);
+        var midleString = allMessage.substring(positionStart, positionEnd);
+        var endString = allMessage.substring(positionEnd, allMessage.length);
+
+        $('#message').val(startString + tag1 + midleString + tag2 + endString);
     }
 });
 myApp.filter('unsafe', function($sce) { return $sce.trustAsHtml; });
--- resources/views/messages/chunks/user/form.blade.php
+++ resources/views/messages/chunks/user/form.blade.php
@@ -3,35 +3,37 @@
     <li><a data-toggle="tab" href="#show">Просмотр</a></li>
 </ul>
 
-<div class="tab-content" ng-app="myApp" ng-controller="MessageController" ng-init="content='test'">
+<div class="tab-content" ng-app="myApp" ng-controller="MessageController" ng-init="content=''">
     <div id="edit" class="tab-pane fade in active">
         <form action="/message/send" method="post">
             <input type="hidden" name="login" id="login" value="<?php echo $user->name ?>"
                    class="form-control"
             />
             <div class="form-group">
-        <textarea ng-keypress="content = replaceSmile()"
-                  ng-model="textContent"
-                  name="message" id="message"
-                  class="form-control"
-                  style=""
-                  id="editor"
-        ></textarea>
+                <div class="btn-group" role="group" aria-label="Опции редактора">
+                    <a class="btn btn-nav" ng-mousedown="bold()"><span class="glyphicon glyphicon-bold"></span></a>
+                    <a class="btn btn-nav" ng-mousedown="italic()"><span class="glyphicon glyphicon-italic"></span></a>
+                    <a class="btn btn-nav" ng-mousedown="strice()"><i class="fa fa-strikethrough"
+                                                                      aria-hidden="true"></i></a>
+                </div>
+                <textarea ng-keypress="content = replaceSmile()"
+                          ng-change="content = replaceSmile()"
+                          ng-model="textContent"
+                          id="message"
+                          class="form-control"
+                          style=""
+                          id="editor"
+                ></textarea>
             </div>
             {{ csrf_field() }}
-            <div class="btn-group" role="group" aria-label="Опции редактора">
-                <a class="btn btn-default"><i class="fa fa-paperclip" aria-hidden="true"></i></a>
-                <a class="btn btn-default"><i class="fa fa-bold" aria-hidden="true"></i></a>
-                <a class="btn btn-default"><i class="fa fa-smile-o" aria-hidden="true"></i></a>
-            </div>
             <div class="form-group ">
                 <button type="submit" class="pull-right btn btn-success">Отправить</button>
             </div>
-
+            <input type="hidden" name="message" id="message_back" value="<?php echo '{{content}}' ?>">
         </form>
     </div>
     <div id="show" class="tab-pane fade">
-        <span  ng-bind-html="content | unsafe"></span>
+        <span ng-bind-html="content | unsafe"></span>
     </div>
 </div>
 <div class="clearfix"></div><br/>
